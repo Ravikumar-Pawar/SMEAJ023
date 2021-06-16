c70d786ea49e1adb71987b53bd2f79fe35a3344c
aravind t <akaravind0000@gmail.com>
2021-06-16 12:15:49 +0530

    mutliplication by aravindt2

---
+++ aravindt2_multiply.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Multiply {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+
+        int number1 = scan.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int number2 = scan.nextInt();
+       
+        int product = num1*num2;
+       
+        System.out.println("Output: "+product);
+    }
+}
