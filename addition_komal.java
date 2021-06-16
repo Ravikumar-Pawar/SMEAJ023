0b43f563b788ad0649053b2981a6e0a3e76bca99
Komal <komalsolanki8585@gmail.com>
2021-06-16 12:10:19 +0530

    addition_manav added

---
+++ addition_komal.java
@@ -0,0 +1,9 @@
+public class Addition_komal{
+	public static void main(String[] args) {
+		int sum=0;
+		for (int i = 0; i < 10; i++) {
+			sum+=i;
+		}
+		System.out.println("Sum Of 1st 10 munbers: "+sum);
+	}
+}
